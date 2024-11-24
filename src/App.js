8ad6bacc674aad8fa806c7547e7be28de2fd3617
Ravi <rs1806105@gmail.com>
2024-11-24 22:58:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         setResponse(null);
 
         try {
-            const res = await fetch("http://localhost:4000/api/point-analyze", {
+            const res = await fetch("http://127.0.0.1:5000/api/point-analyze", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
